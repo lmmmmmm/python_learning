a2eb56ede2e260a9c78a54320edc778a5fb71471
ming.li <lm_20150826@163.com>
2019-01-14 23:30:09 +0800

    添加requests高级方法

--- web/RequestsAdvancedUsage.py
+++ web/RequestsAdvancedUsage.py
@@ -12,3 +12,27 @@ response = requests.get('https://www.baidu.com')
 print(response.cookies)
 for key, value in response.cookies.items():
     print(key + '=' + value)
+
+# 会话维持
+requests.get('http://httpbin.org/cookies/set/number/123456789')
+response = requests.get('http://httpbin.org/cookies')
+print(response.text)
+
+session = requests.Session()
+session.get('http://httpbin.org/cookies/set/number/123456789')
+response = session.get('http://httpbin.org/cookies')
+print(response.text)
+
+# 证书验证
+response = requests.get('https://www.12306.cn')
+print(response.status_code)
+
+# 代理设置
+proxy = {
+    'http': 'http://127.0.0.1:8123',
+    'https': 'https://127.0.0.1:8123'
+}
+response = requests.get('https://www.baidu.com', proxies=proxy)
+print(response.status_code)
+
+
