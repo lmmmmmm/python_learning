3f417013524b3d6ccd94b80926e29bd4518d7418
ming.li <lm_20150826@163.com>
2019-01-28 23:51:43 +0800

    完善代码

--- spider/weixin_articles/Spider.py
+++ spider/weixin_articles/Spider.py
@@ -4,23 +4,23 @@ import requests
 
 base_url = 'https://weixin.sogou.com/weixin?'
 headers = {
-    'Cookie': 'IPLOC=CN6501; SUID=26AE76316119940A000000005C4DBD78; SUV=1548598651162534; ABTEST=0|1548598654|v1; SNUID=65ED34724246C3337AECA090432AD64B; weixinIndexVisited=1; JSESSIONID=aaab_qcBlvvGO9sy7O5Hw; uuidName=1d7e17576a3f4c45b437f767d9be5231; ppinf=5|1548602140|1549811740|dHJ1c3Q6MToxfGNsaWVudGlkOjQ6MTEyMHx1bmlxbmFtZTowOnxjcnQ6MTA6MTU0ODYwMjE0MHxyZWZuaWNrOjA6fHVzZXJpZDoxNzoxMzczMDMxOTE0QHFxLmNvbXw; pprdig=ODdNUbHHOr1d308dX9QwtxXsakZTktj3U7YGLxDEu99KJ3F9CmDU49usRcu7dD4_4iTarISUMXFgfedGUCuTkeJ1Cz2vNCiPBWktREsAAfj5YtZc1ai4_F3fK3hkffTncDDCb4ylNWNUEY9gyEYhgrGM_7bogrcjVM9LjaoLzTw; sgid=10-39010654-AVxNyxzymnCc4eMdmXpg61Q; ppmdig=154860216500000007f389ede82d4261be0bb1818a6519b0; sct=3',
+    'Cookie': 'SUID=26AE76316119940A000000005C4DBD78; SUV=1548598651162534; ABTEST=0|1548598654|v1; SNUID=65ED34724246C3337AECA090432AD64B; weixinIndexVisited=1; IPLOC=US; ppinf=5|1548689836|1549899436|dHJ1c3Q6MToxfGNsaWVudGlkOjQ6MjAxN3x1bmlxbmFtZToxOTpjbGljayVFNCVCOCVCNmNsYWNrfGNydDoxMDoxNTQ4Njg5ODM2fHJlZm5pY2s6MTk6Y2xpY2slRTQlQjglQjZjbGFja3x1c2VyaWQ6NDQ6bzl0Mmx1Skp2LXRGeXd5M0t0aUJWb1FlRVVXd0B3ZWl4aW4uc29odS5jb218; pprdig=lF6ouCd55Ha4-kcUSYCmmRbGzGewzGG9BvQpVUAGzW1uOuu8RHSdURrnFtNDuigmbmW7DIk1eRnqq0TxCPqBLRSy-v581B-SGYVYZxt2nOnhB_gWLA-6bYTcUwYUegQ06b7SgRYSZioZuPm8ekWoiLuo9omYnjXllKBnC8_Dh7Y; sgid=16-39003644-AVxPIaxzqf8DuFWnCyYAuMU; ppmdig=15486898390000009fd8f1f0809c8bea879a838b47a539df; sct=5',
     'Host': 'weixin.sogou.com',
     'Referer': 'https://weixin.sogou.com/',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
 }
 
+proxy_pool_url = 'http://172.0.0.1:8080/get'
 proxy = None
 
 
 def get_proxy():
     try:
-        response = requests.get('http://127.0.0.1:8080/get')
+        response = requests.get(proxy_pool_url)
         if response.status_code == 200:
             return response.text
-        else:
-            return None
+        return None
     except ConnectionError:
         return None
 
@@ -29,9 +29,11 @@ max_count = 5
 
 
 def get_html(url, count=1):
+    print("当前请求代理", proxy)
+    print("当前请求次数", count)
     global proxy
     if count >= max_count:
-        print('请求此时过多')
+        print('请求次数过多')
         return None
     try:
         if proxy:
@@ -47,7 +49,8 @@ def get_html(url, count=1):
             print('302------------>ip被封')
             proxy = get_proxy()
             if proxy:
-                print('using proxy')
+                print('using proxy', proxy)
+                count += 1
                 return get_html(url)
             else:
                 print('get proxy failed')
